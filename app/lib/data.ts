e4db8e5f5bb5f6512f4806d111aaf99c6c7f0a26
PH <ph.ph365@gmail.com>
2026-04-13 17:52:51 -0300

    started navbar

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,7 @@
 import { post } from "@/app/types/blog";
 import postsData from "@/app/data/blog-posts.json";
 
-export async function fetchPosts() {
+export function fetchPosts() {
   try {
     const data: post[] = postsData;
     return { data, message: "Fetch completed" };
