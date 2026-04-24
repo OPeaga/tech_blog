043fd0078d5b894cb0110bfdb41d0cc86a51db16
Ph <Ph.ph365@gmail.com>
2026-04-23 22:58:42 -0300

    implementado a criação de um post e retorno a tela inicial

--- app/services/post-service.ts
+++ app/services/post-service.ts
@@ -2,12 +2,17 @@
 
 import { postRepository } from "@/app/repositories/post-postgres-respository";
 import { crudState } from "@/app/repositories/post-repository";
+import { redirect } from "next/navigation";
 
 export async function createPost(
   prevState: crudState,
   formData: FormData,
 ): Promise<crudState> {
-  return postRepository.createPost(prevState, formData);
+  const result = await postRepository.createPost(prevState, formData);
+
+  !!result.errors && redirect("/"); // sucesso
+
+  return result;
 }
 
 export async function updatePost(
